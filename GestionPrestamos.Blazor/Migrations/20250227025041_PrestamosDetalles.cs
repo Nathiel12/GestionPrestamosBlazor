f33eee6e4cbced7e29fbc955e34e69669660e842
Nathiel12 <nathiel_calderon@ucne.edu.do>
2025-03-01 22:43:28 +0100

    Crear migracion para tabla PrestamosDetalles

---
+++ GestionPrestamos.Blazor/Migrations/20250227025041_PrestamosDetalles.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GestionPrestamos.Migrations
+{
+    /// <inheritdoc />
+    public partial class PrestamosDetalles : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PrestamosDetalles",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CuotaNo = table.Column<int>(type: "int", nullable: false),
+                    PrestamoId = table.Column<int>(type: "int", nullable: false),
+                    Valor = table.Column<double>(type: "float", nullable: false),
+                    Balance = table.Column<double>(type: "float", nullable: false),
+                    Fecha = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PrestamosDetalles", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PrestamosDetalles_Prestamos_PrestamoId",
+                        column: x => x.PrestamoId,
+                        principalTable: "Prestamos",
+                        principalColumn: "PrestamoId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PrestamosDetalles_PrestamoId",
+                table: "PrestamosDetalles",
+                column: "PrestamoId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PrestamosDetalles");
+        }
+    }
+}
